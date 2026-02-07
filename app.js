88c65dfef8b4c0ad1849794fb83cd005c10ba526
newsysteams-boop <newsysteams@gmail.com>
2026-02-07 08:35:15 +0500

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const username = tg.initDataUnsafe.user.first_name;
 
 // Загрузка данных игрока с сервера
 async function loadPlayerData() {
-    const response = await fetch(`http://127.0.0.1:8000/player/${tg_id}?username=${username}`);
+    // Правильный вариант:
+const response = await fetch(`https://fulminic-uropygial-katharyn.ngrok-free.dev/player/${tg_id}?username=${username}`);
     const data = await response.json();
 
     document.getElementById('username').innerText = data.username;
